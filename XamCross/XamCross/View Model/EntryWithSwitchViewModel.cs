1c923b0c8907532b5d6f9c8b7abbf848ecfea219
Vikas <21singhvikas@gmail.com>
2018-12-19 17:43:26 +0530

    Binding Entry Control with Switch

---
+++ XamCross/XamCross/View Model/EntryWithSwitchViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamCross.View_Model
+{
+    public class EntryWithSwitchViewModel
+    {
+        public string PersonName { get; set; }
+        public string WorksIn { get; set; }
+
+    }
+}
