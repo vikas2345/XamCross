2eb595f8c2de78b638b49575b3a0f9812adc50e8
Vikas <21singhvikas@gmail.com>
2019-01-04 14:18:35 +0530

    DB Connection with XF

---
+++ XamCross/XamCross.Android/Data/SQLite_Droid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using XamCross.Data;
+
+namespace XamCross.Droid.Data
+{
+    public class SQLite_Droid : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFileName = "Testdb.db3";
+            string documentPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+            var path = Path.Combine(documentPath, sqliteFileName);
+        }
+    }
+}
